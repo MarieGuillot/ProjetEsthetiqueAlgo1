ea55cf70f65f392ddb7feba350d0ff0bb4c5aaeb
Marie Guillot <guillot-marie@hotmail.fr>
2021-03-07 19:35:48 +0100

    test

--- build/build.js
+++ build/build.js
@@ -2,30 +2,51 @@ var gui = new dat.GUI();
 var params = {
     Seed: 0,
     Long: -30,
+    Angle: 0.39,
     Download_Image: function () { return save(); },
 };
 gui.add(params, "Seed", 0, 255, 1);
-gui.add(params, "Long", -100, 0, 1);
+gui.add(params, "Long", -1000, 0, 1);
+gui.add(params, "Angle", 0, 1.7, 0.001);
 gui.add(params, "Download_Image");
 function smallLine() {
-    line(0, 0, 0, params.Long);
+    var j = 0;
+    while (j < 2) {
+        line(0, 0, 0, params.Long);
+        push();
+        var angle = ((PI / 12) - j * (PI / 7));
+        rotate(angle);
+        line(0, 0, 0, (2 - j) * (2 / 3) * params.Long);
+        pop();
+        translate(0, params.Long);
+        j++;
+    }
+    line(0, 0, 0, (1 / 3) * params.Long);
+    translate(0, (1 / 3) * params.Long);
 }
 function divisePlant(stop) {
     push();
-    var maxI = random(0, 3);
-    for (var i = 0; i < maxI; i++) {
-        push();
-        var angle = random(-PI / 8, PI / 8);
+    if (stop > 1) {
+        var angle = random(-1 * params.Angle, params.Angle);
         rotate(angle);
         smallLine();
-        translate(0, params.Long);
-        if (stop > 1) {
-            push();
-            stop--;
-            divisePlant(stop);
-            pop();
-        }
+        push();
+        divisePlant(stop - 1);
         pop();
+        push();
+        divisePlant(stop - 2);
+        pop();
+    }
+    if (stop == 0) {
+        var angle = random(-1 * params.Angle, params.Angle);
+        rotate(angle);
+        smallLine();
+    }
+    if (stop == 1) {
+        var angle = random(-1 * params.Angle, params.Angle);
+        rotate(angle);
+        smallLine();
+        smallLine();
     }
     pop();
 }
@@ -33,10 +54,12 @@ function draw() {
     randomSeed(params.Seed);
     background("#F4EAD3");
     noFill();
+    var B = 255;
     stroke("#136428");
     translate(width / 2, 4 * height / 5);
     push();
-    divisePlant(15);
+    var longueurArbre = 5;
+    divisePlant(longueurArbre);
 }
 function setup() {
     p6_CreateCanvas();
--- src/sketch.ts
+++ src/sketch.ts
@@ -6,10 +6,12 @@ const gui = new dat.GUI()
 const params = {
     Seed: 0,
     Long : -30,
+    Angle : 0.39,
     Download_Image: () => save(),
 }
 gui.add(params, "Seed", 0, 255, 1)
-gui.add(params, "Long", -100, 0, 1)
+gui.add(params, "Long", -1000, 0, 1)
+gui.add(params, "Angle", 0, 1.7, 0.001)
 gui.add(params, "Download_Image")
 
 // -------------------
@@ -17,38 +19,78 @@ gui.add(params, "Download_Image")
 // -------------------
 
 function smallLine () {
-    line(0,0,0,params.Long)
+
+    let j = 0;
+    while (j<2) {
+        line(0,0,0,params.Long); 
+        push();
+        let angle = ((PI/12) - j*(PI/7));
+        rotate(angle); 
+        line(0,0,0,(2-j)*(2/3)*params.Long); 
+        pop();
+        translate(0,params.Long);
+        j++;
+    }   
+    line(0,0,0,(1/3)*params.Long);
+    translate(0,(1/3)*params.Long);
+    
 }
+/*
+function fibonacci (n) {
+    if (n<=1) {
+        return n;
+    } 
+    else {
+        return fibonacci(n-1)+fibonacci(n-2);
+    }
+
+}*/
 
 function divisePlant (stop) {
+    
     push();
-    let maxI = random(0,3);
-    for (let i=0; i< maxI; i++) {
-        push();
-        let angle = random(-PI/8, PI/8);
-        rotate(angle);
-        smallLine();
-        translate(0,params.Long);
+        
         if (stop>1) {
+
+            let angle = random(-1*params.Angle, params.Angle);
+            rotate(angle);
+            smallLine();
+
             push();
-            stop--;
-            divisePlant(stop);
-            pop();
+            divisePlant(stop-1);
+            pop();  
+            push();
+            divisePlant(stop-2);
+            pop(); 
+
         }
-        pop();
-    }
+        if (stop==0) {
+            let angle = random(-1*params.Angle, params.Angle);
+            rotate(angle);
+            smallLine();
+        }
+        if (stop==1) {
+            let angle = random(-1*params.Angle, params.Angle);
+            rotate(angle);
+            smallLine();
+            smallLine();
+        }
+    
     pop();
 }
+
 
 function draw() {
     
     randomSeed(params.Seed);
     background("#F4EAD3");
     noFill();
+    let B=255;
     stroke("#136428");
     translate(width/2, 4*height/5);
     push();
-    divisePlant(15);
+    let longueurArbre = 5;
+    divisePlant(longueurArbre);
     
 
 }
